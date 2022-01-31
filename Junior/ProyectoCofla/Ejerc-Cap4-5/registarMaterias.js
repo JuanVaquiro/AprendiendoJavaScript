ede86c67ef3be1b7cef3972ca930623619afcd10
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-01-31 17:08:45 -0500

    ejercio pratica

---
+++ Junior/ProyectoCofla/Ejerc-Cap4-5/registarMaterias.js
@@ -0,0 +1,58 @@
+const obtnerInfo = (materia) => {
+  mate = {
+      fisica: ['profe lucas', 'pedro', 'maria', 'cofla'], 
+      programacion: ['profe vaqui', 'pedro', 'juan', 'maria', ],
+      logica: ['profe angela', 'pedro', 'juan', 'maria', 'cofla'],
+      quimica: ['profe daniel', 'pedro', 'juan',  'cofla']
+  }
+  if (mate[materia] !== undefined) {    
+      return [ mate[materia],materia,mate ];
+  } else{
+      return mate;
+  }
+};
+
+const mostrarInfo = (materia) => {
+    let infos = obtnerInfo(materia);
+    if (infos !== false) {
+    const profesor = obtnerInfo(materia)[0][0];
+    const alumnos = obtnerInfo(materia)[0];
+    alumnos.shift();
+    document.write(`El profesor de <b>${infos[1]}</b>  
+    es: <b style="color:orange"> ${profesor}</b><br>  
+    Almunos registrados: <b style="color:Fuchsia">${alumnos}</b><br><br>`);
+    };
+};
+
+const cantidadClase = (almuno) => {
+  let informacion = obtnerInfo();
+  let clasesPresentes = [];
+  let cantidadTotal = 0;
+  for ( let info in informacion ) {
+    if ( informacion[info].includes(almuno) ) { 
+        cantidadTotal ++;
+        clasesPresentes.push(" " + info);
+    }
+  } return `<b style="color:Fuchsia"> ${almuno} 
+  </b> esta en ${cantidadTotal} clases<br>
+  Esta cursnado las clases <b style='color:LimeGreen'>${clasesPresentes}</b><br><br>`;
+};
+
+
+
+mostrarInfo('fisica');
+mostrarInfo('logica');
+document.write(cantidadClase('cofla'))
+
+
+//objeto array
+const materias1 = [{
+    fisica: ['pedro', 'maria', 'cofla'], 
+    programacion: ['pedro', 'juan', 'maria', ],
+    logica: ['pedro', 'juan', 'maria', 'cofla'],
+    quimica: ['pedro', 'juan',  'cofla']
+}];
+
+const arrayMap = materias1.map(materias1 => materias1.fisica)
+document.write(`alumnos registrados: ` + arrayMap);
+console.log(arrayMap);
