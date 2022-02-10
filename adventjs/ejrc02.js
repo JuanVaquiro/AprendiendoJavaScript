4a7bc3f42e752252ad318d50d2418a4681b012e2
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-02-09 21:59:42 -0500

    ejrc adventjs part2

--- adventjs/ejrc02.js
+++ adventjs/ejrc02.js
@@ -1,21 +1,48 @@
 const carta = '{bici coche balón _playstation bici coche peluche}'
 
-let objeto = JSON.parse(carta);
-console.log(objeto);
+function listGifts(letter) {
+  let giftList = letter.split(" ");
+  giftList = giftList.filter((item) => item);
+  console.log(giftList);
+  const resultObject = {};
+  let currectElement = "";
+  let counter = 0;
 
-// function listGifts(letter) {
-//     // ¡Tú puedes!
-//     return {}
-//    }
+  giftList.map((gift) => {
+    currectElement = gift;
+    counter = 0;
+    if (!currectElement.includes("_")) {
+      giftList.map((interator) => {
+        if (currectElement === interator) {
+          counter += 1;
+        }
+        resultObject[`${currectElement}`] = counter;
+      });
+    }
+  });
 
-// const regalos = listGifts(carta)
-
-console.log(regalos)
-/*
-{
-  bici: 2,
-  coche: 2,
-  balón: 1,
-  peluche: 1
+  return resultObject;
 }
-*/
+
+const regalos = listGifts(carta)
+console.log(regalos)
+
+
+function listGifts2(letter) {
+  let palabras = letter.split(" ");
+  palabras = palabras.filter((item) => item);
+  let lista = {};
+
+  for (let i = 0; i < palabras.length; i++) {
+    if (!palabras[i].includes("_")) {
+      if (lista[palabras[i]] == undefined) {
+        lista[palabras[i]] = 1;
+      } else {
+        lista[palabras[i]] += 1;
+      }
+    }
+  }
+  return lista;
+}
+
+console.log(listGifts2(carta));
