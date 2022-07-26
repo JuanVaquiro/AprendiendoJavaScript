d5ea1e78faeaa4fa84105d3390bb1cf94e82a252
JuanVaquiro <jvaquiro23@itfip.edu.co>
2022-07-26 11:23:01 -0500

    Don't repeat yourself varibale const global

--- Mid/PlatziConten/mokemon/script.js
+++ Mid/PlatziConten/mokemon/script.js
@@ -1,42 +1,46 @@
-let playerAtack;
-let enemyAtack;
-let playerLifes = 3;
-let enemylifes = 3;
-let resultCombat;
+const sectionAtackSelect = document.getElementById("atack-select")
+const sectionrRestart = document.getElementById("restart-select")
+const btnPlayer = document.getElementById("btn-animalSelect")
+const btnFire = document.getElementById("btn-fire")
+const btnWater = document.getElementById("btn-water")
+const btnLand = document.getElementById("btn-land")
+const btnRestart = document.getElementById("btn-restart")
+
+const sectionMonsterkSelect = document.getElementById("monster-select");
+const playerSpan = document.getElementById("monster-player");
+const inpuntDrangpo = document.getElementById("Dragonpo");
+const inpuntLionpo = document.getElementById("Cocodripo");
+const inpuntCocodripo = document.getElementById("Ardispo");
+
+const enemySpan = document.getElementById("monster-enemy");
+
+const spanLifePlayer = document.getElementById("life-player");
+const spanLifeEnemy = document.getElementById("life-enemy");
+
+const sectionMessage = document.getElementById("result")
+const msmPlayerAtack = document.getElementById("msm-player-atack");
+const msmEnemyAtack = document.getElementById("msm-enemy-atack")
+
+let playerAtack
+let enemyAtack
+let playerLifes = 3
+let enemylifes = 3
+let resultCombat
 
 function startGame() {
-  // ocultar section
-  let sectionAtackSelect = document.getElementById("atack-select");
   sectionAtackSelect.style.display = "none";
-
-  let sectionrRestart = document.getElementById("restart-select");
   sectionrRestart.style.display = "none";
 
-  let btnPlayer = document.getElementById("btn-animalSelect");
   btnPlayer.addEventListener("click", selectMonsterPlayer);
-
-  let btnFire = document.getElementById("btn-fire");
   btnFire.addEventListener("click", fireAtack);
-  let btnWater = document.getElementById("btn-water");
   btnWater.addEventListener("click", waterAtack);
-  let btnLand = document.getElementById("btn-land");
   btnLand.addEventListener("click", landAtack);
-
-  let btnRestart = document.getElementById("btn-restart");
   btnRestart.addEventListener("click", gameRestart);
 }
 
 function selectMonsterPlayer() {
-  let sectionMonsterkSelect = document.getElementById("monster-select");
   sectionMonsterkSelect.style.display = "none";
-
-  let sectionAtackSelect = document.getElementById("atack-select");
   sectionAtackSelect.style.display = "flex";
-
-  let inpuntDrangpo = document.getElementById("Dragonpo");
-  let inpuntLionpo = document.getElementById("Cocodripo");
-  let inpuntCocodripo = document.getElementById("Ardispo");
-  let playerSpan = document.getElementById("monster-player");
 
   if (inpuntDrangpo.checked) {
     playerSpan.innerHTML = "Dragonpo";
@@ -46,10 +50,7 @@ function selectMonsterPlayer() {
     playerSpan.innerHTML = "Ardispo";
   } else {
     alert("Oops!! algo salio Mal \n Porfavor selecciona un Monstermon");
-    let sectionAtackSelect = document.getElementById("atack-select");
     sectionAtackSelect.style.display = "none";
-
-    let sectionMonsterkSelect = document.getElementById("monster-select");
     sectionMonsterkSelect.style.display = "flex";
   }
   selectMonsterEnemy();
@@ -57,7 +58,6 @@ function selectMonsterPlayer() {
 
 function selectMonsterEnemy() {
   let randomMonster = random(1, 3);
-  let enemySpan = document.getElementById("monster-enemy");
 
   if (randomMonster == 1) {
     enemySpan.innerHTML = "Dragonpo";
@@ -99,9 +99,7 @@ function enemyRandomAtack() {
 }
 
 function combat() {
-  let spanLifePlayer = document.getElementById("life-player");
-  let spanLifeEnemy = document.getElementById("life-enemy");
-
+  
   if (playerAtack == enemyAtack) {
     resultCombat = "EMPATE";
   } else if (playerAtack == "FUEGO 🔥" && enemyAtack == "TIERRA 🌱") {
@@ -126,12 +124,10 @@ function combat() {
 
 function playerWin() {
   if (playerLifes == 0) {
-    let sectionrRestart = document.getElementById("restart-select");
     sectionrRestart.style.display = "block";
     createMessageGameOver("Opps!! 😢 haz perdido la partida 😢 FIN JUEGO")
     disableButton();
   } else if (enemylifes == 0) {
-    let sectionrRestart = document.getElementById("restart-select");
     sectionrRestart.style.display = "block";
     createMessageGameOver("Huurra!! 🏆 haz ganado la partida 🏆 FIN JUEGO")
     disableButton();
@@ -139,19 +135,12 @@ function playerWin() {
 }
 
 function disableButton() {
-  let btnFire = document.getElementById("btn-fire");
   btnFire.disabled = true;
-  let btnWater = document.getElementById("btn-water");
   btnWater.disabled = true;
-  let btnLand = document.getElementById("btn-land");
   btnLand.disabled = true;
 }
 
-function createMessage() {
-  let sectionMessage = document.getElementById("result")
-  let msmPlayerAtack = document.getElementById("msm-player-atack");
-  let msmEnemyAtack = document.getElementById("msm-enemy-atack");
-  
+function createMessage() { 
   let paragraphPlayer = document.createElement('p')
   let paragraphEnemy = document.createElement('p')
 
@@ -165,7 +154,6 @@ function createMessage() {
 }
 
 function createMessageGameOver(finalResult) {
-  let sectionMessage = document.getElementById("result")
   sectionMessage.innerHTML = finalResult
 }
 
