fa1c31c3c2d076f320858f86ac28258074801560
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-05-22 20:13:26 -0500

    nombrar según el tipo de dato

---
+++ CleanCode-JS/nomenclatura.js
@@ -0,0 +1,54 @@
+/*** Cómo nombrar según el tipo de dato ***/
+
+// regular
+const userList = ['alex', 'juan', 'pedro']
+
+// bueno
+const users = ['alex', 'juan', 'pedro']
+
+/* ARRAYS */
+// exelecte
+// const userNames = ['alex', 'juan', 'pedro']
+
+/* BOOLEANOS */
+const isValidd = true // <- esto es valido? 
+const canRead = false // <- puede ser valido?
+const hasColor = true // <- contiene valor valido?
+
+/* NUMERICOS */
+const maxUsers = 50
+const minUsers = 10
+const totalUsers = 23
+
+/* FUNCIONES */
+// representa una accion, Verbo + Sustativo
+creareUser()
+updateUser()
+sendEmial()
+// acceso (GET), modificar(SET), 
+
+/* CLASES  con resposavilidades unicas */
+
+/* PRACTICA */
+
+const numbers = [1, 2, 3, 4, 5]
+const userNames = ['Cande', 'Alex', 'Mariana']
+const colorNames = ['red', 'green', 'blue']
+
+const isActive = true
+const isCorrect = false
+const canWrite = true
+const hasUser = false
+
+let totalResult = 3543
+const eje_Y = 20
+const PI = 3.14159
+
+sendAlert()
+createObject()
+setPassword()
+getResults()
+
+class School { }
+class Service { }
+class Account { }
