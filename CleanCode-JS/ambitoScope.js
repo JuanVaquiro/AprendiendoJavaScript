0245ba09c948dd179b32be63b3bdd4fff1c58752
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-05-24 19:54:14 -0500

    ambitioScope

---
+++ CleanCode-JS/ambitoScope.js
@@ -0,0 +1,18 @@
+const age = 23
+
+function printAge() {
+  console.log(age)
+}
+
+function mainApp() {
+  const age = 20
+  printAge()
+}
+
+mainApp()
+
+/* 
+  HOISTING 
+  DECLARACION variables y funciones
+  En el tiempo de copilacion movida hacia arriba de su ambito
+*/
